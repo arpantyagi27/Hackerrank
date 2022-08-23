19573255831961a6bd622a486ec57275b397d4e9
Arpan <at5318@srmist.edu.in>
2022-08-24 02:12:43 +0530

    Solution for Simple Array Sum
    :q
    exit
    
    

---
+++ Simple Array Sum.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+    int sum;
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+    } 
+    cout<<sum;   
+    return 0;
+}
